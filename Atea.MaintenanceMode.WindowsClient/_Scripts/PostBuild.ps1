583bead9acf014851afbab643a8b115357ca87fc
Samuel Tegenfeldt <samuel.tegenfeldt@atea.se>
2016-11-03 17:02:20 +0100

    Build scripts adjusted, not using Auto Deploy to copy files to release folder.

--- Atea.MaintenanceMode.WindowsClient/_Scripts/PostBuild.ps1
+++ Atea.MaintenanceMode.WindowsClient/_Scripts/PostBuild.ps1
@@ -1,6 +1,9 @@
 param (
-    [string] $FilePath
+    [string] $FilePath,
+    [string] $SolutionFolder
 )
 
-#signtool sign /tr http://timestamp.digicert.com /td sha256 /fd sha256 /sha1 5E1D01C6F02199D1A39D806EB17A961F5C476EE2 "$(OutDir)$(TargetFileName)"
+$buildFolder = [string] "$($FilePath.Substring(0,$FilePath.LastIndexOfAny("\")))\"
+$releaseFolder = [string] "$SolutionFolder\Release\Windows Client\"
 Set-AuthenticodeSignature -Certificate (dir Cert:\CurrentUser\My\5E1D01C6F02199D1A39D806EB17A961F5C476EE2) -TimestampServer "http://timestamp.digicert.com" -FilePath "$($FilePath)"
+
